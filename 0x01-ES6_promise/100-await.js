933b33416e619bc85e3d9997a482b32cec9ff241
AthmanDodoe <jilloathman943@gmail.com>
2023-01-31 12:21:05 +0300

    0x01-ES6_promise

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,14 @@
-/* eslint-disable no-unused-vars */
-// eslint-disable-next-line import/extensions
-import { async } from 'rsvp';
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let res = {};
-
+  let photo;
+  let user;
   try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    res = { photo, user };
-  } catch (err) {
-    res = { photo: null, user: null };
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (error) {
+    photo = null;
+    user = null;
   }
-  return res;
+  return { photo, user };
 }
