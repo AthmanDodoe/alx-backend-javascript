bd01677b3c12f11fd93aeb62e9b64478b9259d8b
AthmanDodoe <jilloathman943@gmail.com>
2023-01-31 22:26:30 +0300

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+let nbrOfQueries = 0;
+export const weakMap = new WeakMap();
+
+export const queryAPI = (endpoint) => {
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  nbrOfQueries += 1;
+  weakMap.set(endpoint, nbrOfQueries);
+};
